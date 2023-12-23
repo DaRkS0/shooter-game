7be28081e2fdc5c5d0862a92bd2727fc01dbbb0b
Your Name <you@example.com>
2023-12-24 01:10:19 +0200

    first commit

---
+++ src/lib/GameObject.js
@@ -0,0 +1,38 @@
+/**
+ * @typedef {{x:number,y:number}} velocity
+ * @typedef {{x:number,y:number,radius:number,color:string,velocity:velocity}} GameObjectData
+ */
+
+class GameObject {
+    /**
+     * 
+     * @param {GameObjectData} data 
+     */
+    constructor(data) {
+        this.x = data.x;
+        this.y = data.y;
+        this.color = data.color;
+        this.radius = data.radius;
+        this.velocity = data.velocity;
+    }
+
+    draw() {
+        if (this.ctx) {
+            this.ctx.beginPath();
+            this.ctx.fillStyle = this.color;
+            this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            this.ctx.fill()
+        }
+    }
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+    update(ctx) {
+        this.ctx = ctx;
+        this.draw();
+
+    }
+}
+
+export { GameObject }
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { GameObject } from "$lib/GameObject";
+  import { onMount } from "svelte";
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  let canv;
+
+  onMount(() => {
+    // @ts-ignore
+    const context = canv.getContext("2d");
+    if (context) {
+      canv.width = innerWidth;
+      canv.height = innerHeight;
+      const player = new GameObject({
+        x: canv.width / 2,
+        y: canv.height / 2,
+        color: "blue",
+        radius: 20,
+        velocity: { x: 0, y: 0 },
+      });
+      player.update(context);
+    }
+  });
+</script>
+
+<div class="wrapper h-screen w-screen">
+  <canvas class="h-full w-full" bind:this={canv}> </canvas>
+</div>
