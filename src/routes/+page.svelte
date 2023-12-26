c36a06552eedb974a0efd259fcf285bfaa63c369
Your Name <you@example.com>
2023-12-26 13:48:38 +0200

    phaser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { GameObject } from "$lib/GameObject";
+  import { Start } from "$lib/Game";
   import { GameManager } from "$lib";
   import { onMount } from "svelte";
   /**
@@ -13,39 +13,8 @@
   let manager;
   let frameKey = -1;
   onMount(() => {
-    // @ts-ignore
-    const context = canv.getContext("2d");
-    if (context) {
-      canv.width = innerWidth;
-      canv.height = innerHeight;
-      manager = new GameManager(context);
-      Start();
-
-      //   const player = new GameObject({
-      //     x: canv.width / 2,
-      //     y: canv.height / 2,
-      //     color: "blue",
-      //     radius: 20,
-      //     velocity: { x: 0, y: 0 },
-      //   });
-      //   player.update(context);
-    }
+    Start();
   });
-  function Start() {
-    frameKey = requestAnimationFrame(animate);
-  }
-
-  function animate() {
-    frameKey = requestAnimationFrame(animate);
-    manager?.Animate();
-  }
 </script>
 
-<div class="wrapper h-screen w-screen">
-  <canvas
-    class="h-full w-full"
-    bind:this={canv}
-    on:click={(ev) => manager.AddProjectile(ev.clientX, ev.clientY)}
-  >
-  </canvas>
-</div>
+<div class="wrapper h-screen w-screen"></div>
